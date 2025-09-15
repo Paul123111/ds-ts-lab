967aa551b9ae4fb4dabb2476fa8a3c6c3b6d8594
Paul123111 <20102629wit@gmail.com>
2025-09-15 12:24:56 +0100

    Typed function signature.

---
+++ src/02-functions.ts
@@ -0,0 +1,48 @@
+import {Friend, Colleague } from './myTypes'
+import {friends, colleagues} from './01-basics'
+
+function older(f: Friend) : string {
+  f.age += 1
+  return `${f.name} is now ${f.age}` 
+}
+
+//console.log(older(friends[0]))
+
+// Increment the age of an array of friends
+function allOlder(friends: Friend[]) : string[] {
+  let list: string[] = new Array(friends.length)
+  let count: number = 0
+  friends.forEach( (f) => {
+    f.age += 1
+    list[count]=`${f.name} is now ${f.age}`
+    count++
+  });
+  return list
+}
+//console.log(allOlder(friends))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+//console.log(highestExtension(colleagues.current));
+
+function addColleague(cs: Colleague[], name: string, department: string, email: string) : void {
+  const newColleague: Colleague = {
+    name: name,
+    department: department,
+    contact: {
+      email: email,
+      extension: highestExtension(cs).contact.extension+1,
+    },
+  };
+  cs.push(newColleague)
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+
